7ef7b2ab32a06e88f76e3923dd7ae951ade38212
Amatey1 <55081775+Amatey1@users.noreply.github.com>
2021-12-16 21:54:00 +0200

    Add files via upload

---
+++ summativeQ4.py
@@ -0,0 +1,26 @@
+# this algorithm takes a number and adds all the indivual values within the string to form a sum 
+# the number of times the number should be concatenated is represented by K
+# the goal is to add the concatnated values till you have jus 1 value .
+
+def megadigit(n,k):
+
+    p = k*n
+    #This multiplies the number of string by the number of times k create a duplication k number of times    
+    while len(p) >= 2 :       
+        # since super digit aims to get to the point where the sum of the concatnated values(P) is 1 value
+        #  we use a while loop keep reducing the number of digits in P.
+        sum = 0
+        # loops through all the values of P and converts them to integer  as they are added up.
+        for g in range(len(p)):
+            sum = sum + int(p[g])
+        p = str(sum)
+    return p
+
+
+print(megadigit("23",2))
+print (megadigit("54",3))
+print (megadigit("6234",4))
+
+
+
+

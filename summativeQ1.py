a6ad88035162cbd38b7458cede74c56a7f712614
Amatey1 <55081775+Amatey1@users.noreply.github.com>
2021-12-16 20:00:12 +0200

    Update summativeQ1.py

--- summativeQ1.py
+++ summativeQ1.py
@@ -5,7 +5,7 @@ def summer(n):
  total = 0
 
 # this loop goes through each value between 0 and the "n" adding the value of the index it is currently on to the "total" variable.
-#The code stops once it reaches n and returns the sum.
+#The code stops once it reaches the value containted within n and returns the sum.
  for x in range(n+1):
      total=total+x
  return total
